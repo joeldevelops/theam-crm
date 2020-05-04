10595e4a0b737869613aefc9290e97f1b50fce3c
Joel Milligan <joelsaltwater@gmail.com>
2020-05-04 12:25:07 -0700

    Remove unused code

--- customer-service/src/app.module.ts
+++ customer-service/src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 
 @Module({
     // TODO: Add the mongo uri as a config variable
   imports: [
-      MongooseModule.forRoot('mongodb://localhost/customer'),
-      CustomerModule
-    ],
-  controllers: [AppController],
-  providers: [AppService],
+    MongooseModule.forRoot('mongodb://localhost/customer'),
+    CustomerModule
+  ]
 })
 export class AppModule {}
